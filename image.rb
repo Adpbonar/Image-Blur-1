795ba9043f1e0c0c1ad6b9858c5c255c89bf0baf
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-25 03:18:34 +0000

    Image Blur #1

--- image.rb
+++ image.rb
@@ -1,7 +1,25 @@
+class Image
+  attr_reader :image
+
+  def initialize(table)
+   @image = table
+  end
+  
+  def output_image
+    @image.each do |arr|
+      puts arr.join(" ")
+    end
+  end
+end
+  
+
+
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+
+
 image.output_image
